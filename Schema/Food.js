6f0f4aa011479f73742ce530a3f6b157722dc315
mg4506 <munish4506.be21@chitkara.edu.in>
2024-07-06 14:22:48 +0530

    test

--- Schema/Food.js
+++ Schema/Food.js
@@ -1,12 +1,13 @@
 const mongoose=require('mongoose')
 
 const FoodSchema=new mongoose.Schema({
-   FoodImage:String,
-   FoodName:String,
-   FoodMRP:String,
-   FoodPrice:String,
-   FoodType:String,
-    status: String
+  "image":String,
+    "title":String,
+    "Mrp":String,
+    "price":String,
+    "type":String,
+    "description":String,
+    "status":String
 })
 const FoodModel=mongoose.model("FoodDetail",FoodSchema)
 module.exports=FoodModel
